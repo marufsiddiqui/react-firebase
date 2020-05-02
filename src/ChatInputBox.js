d0fc62cda86c2f896eb3b709f4a865155f8e97a5
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2020-05-02 23:55:11 +0200

    Add message functionality

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { db } from './firebase';
 
 export const ChanelInputBox = () => {
   return (
-    <form className="ChatInputBox">
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        const value = event.target.elements[0].value;
+        db.collection('channels').doc('general').collection('messages').add({
+          text: value,
+          createdAt: new Date(),
+        });
+        event.target.reset();
+      }}
+      className="ChatInputBox"
+    >
       <input className="ChatInput" placeholder="Message #general" />
     </form>
   );
