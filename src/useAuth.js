e8d690d377d4a2c2c66a1f54a6d24133c78ec167
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2020-05-05 01:04:53 +0200

    Lecture 39: Save user to firestore

--- src/useAuth.js
+++ src/useAuth.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
-import { firebase } from './firebase';
+import { firebase, db } from './firebase';
 
 export function useAuth() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    return firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        setUser({
-          displayName: user.displayName,
-          photoUrl: user.photoURL,
-          uid: user.uid,
-        });
+    return firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        const user = {
+          displayName: firebaseUser.displayName,
+          photoUrl: firebaseUser.photoURL,
+          uid: firebaseUser.uid,
+        };
+        setUser(user);
+        db.collection('users').doc(user.uid).set(user, { merge: true });
       } else {
         setUser(null);
       }
