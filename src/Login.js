108f14af7735a058e39a4c5f55d71f3c6c16b607
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2020-05-05 00:58:35 +0200

    Lecture 38: Auth Error Handling

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { firebase } from './firebase';
 
 function Login() {
+  const [authError, setAuthError] = useState(null);
   const handleSignIn = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    await firebase.auth().signInWithPopup(provider);
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (e) {
+      setAuthError(e);
+    }
   };
 
   return (
     <div className="Login">
       <h1>Chat</h1>
       <button onClick={handleSignIn}>Sign in with Google</button>
+      {authError && (
+        <div>
+          <p>Sorry, there was a problem</p>
+          <p>
+            <i>{authError.message}</i>
+          </p>
+          <p>Please try again</p>
+        </div>
+      )}
     </div>
   );
 }
