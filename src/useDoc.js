255ebee59d6f83a2300d19172c2a1a1686f386c7
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2020-05-06 23:03:55 +0200

    Lecture 49: Avoid flickering with a simple cache

--- src/useDoc.js
+++ src/useDoc.js
@@ -1,25 +1,36 @@
 import { useEffect, useState } from 'react';
 import { db } from './firebase';
 
+const cache = {};
+const pendingCache = {};
+
 export const useDoc = path => {
-  const [doc, setDoc] = useState(null);
+  const [doc, setDoc] = useState(cache[path]);
 
   useEffect(() => {
+    if (doc) {
+      return;
+    }
+
     let stillMounted = true;
-    db.doc(path)
-      .get()
-      .then(doc => {
-        if (stillMounted) {
-          setDoc({
-            ...doc.data(),
-            id: doc.id,
-          });
-        }
-      });
+    const pending = pendingCache[path];
+    const promise = pending || (pendingCache[path] = db.doc(path).get());
+
+    promise.then(doc => {
+      if (stillMounted) {
+        const user = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        setDoc(user);
+        cache[path] = user;
+      }
+    });
+
     return () => {
       stillMounted = false;
     };
-  }, [path]);
+  }, [doc, path]);
 
   return doc;
 };
